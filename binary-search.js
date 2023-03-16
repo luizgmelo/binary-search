e208ec39e6207fc883ae121f6a0a8cafe95ce700
luizgmelo <88911920+luizgmelo@users.noreply.github.com>
2023-03-16 13:51:17 -0300

    create selection sort algorithm

--- binary-search.js
+++ binary-search.js
@@ -1,4 +1,4 @@
-list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 
+const list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 
     11, 12, 13, 14, 15, 16, 17, 18, 19, 
     20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 
     30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 
@@ -9,11 +9,46 @@ list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10,
     80, 81, 82, 83, 84, 85, 86, 87, 88, 89,
     90, 91, 92, 93, 94, 95, 96, 97, 98, 99, 100]
 
-secondList = [0, 1, 40, 44, 53, 62, 80, 92, 99, 123, 124]
+const secondList = [0, 1, 40, 44, 53, 62, 80, 92, 99, 123, 124]
 
-thirdList = ['Ana', 'Carlos', 'Dora', 'Guilherme', 'Ingrid', 
+const thirdList = ['Ana', 'Carlos', 'Dora', 'Guilherme', 'Ingrid', 
 'Joelma', 'Joelson', 'Kaio', 'Leonardo', 'Lucas', 'Marcos', 
-'Orlando', 'Richard', 'Vitoria', 'Willamy']
+'Orlando', 'Richard', 'Sabrina', 'Willamy']
+
+// unordered lists
+
+const fourList = [22, 14, 55, 21, 11, 3, 1, 11, 2, 5, 6, 8, 9, 3, 2]
+
+const fiveList = [5, 4, 3, 2, 1]
+
+const sixList = ["Zebra", "Wolf", "Lion", "Monkey", "Cat"]
+
+function searchLow(list, length) {
+    let lowIndex = 0
+    let low = list[lowIndex]
+
+    for (let i = 0; i < length; i++) {
+        if (list[i] < low) {
+            lowIndex = i
+            low = list[lowIndex]
+        }
+    }
+    return lowIndex
+}
+
+function selectionSort(list) {
+    const newList = []; 
+    const listLength = list.length
+
+    for (let j = 0; j < listLength; j++) {
+        let index = searchLow(list, listLength)
+        let lowElement = list.splice(index, 1)[0]
+        newList.push(lowElement)  
+    }
+    return newList
+}  
+
+console.log(selectionSort(sixList))
 
 function binarySearch(list, target) {
     let left = 0
@@ -35,7 +70,7 @@ function binarySearch(list, target) {
     }
     return `${target} don't exists`
 }
-    
+
 console.log(binarySearch(list, 123))
 console.log(binarySearch(secondList, 80))
-console.log(binarySearch(thirdList, "Leonardo"))
+console.log(binarySearch(thirdList, "Sabrina"))
