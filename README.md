2f484a3b9602da914489d0e7d96a6a9976f0afe3
luizgmelo <88911920+luizgmelo@users.noreply.github.com>
2023-03-22 20:48:59 -0300

    deleted examples and update README

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Binary Search
+# Binary Search And Selection Sort
 Algorithm binary search
 
 # What is Binary Search?
@@ -6,6 +6,13 @@ Algorithm binary search
 this divide the array in the middle until the middle element is the element that you want
 to find, if this element don't be found, the low position (or left position) in array is greater then
 high, so this function return "the element don't exists", otherwise it finds the position of the element and returns.
+
+# What is a Selection Sort?
+- Is a Algorithm for sort a list.
+- Complexity: O(n²)
+- Selection sort consists in read a list and create another
+list to add elements in order.
+
 
 ## Be freedom to contribute and made your own binary search with your favorite language programming 
 - Fork this and send a pull request
--- binary-search.js
+++ binary-search.js
@@ -1,28 +1,3 @@
-const list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 
-    11, 12, 13, 14, 15, 16, 17, 18, 19, 
-    20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 
-    30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 
-    40, 41, 42, 43, 44, 45, 46, 47, 48, 49,
-    50, 51, 52, 53, 54, 55, 56, 57, 58, 59,
-    60, 61, 62, 63, 64, 65, 66, 67, 68, 69,
-    70, 71, 72, 73, 74, 75, 76, 77, 78, 79,
-    80, 81, 82, 83, 84, 85, 86, 87, 88, 89,
-    90, 91, 92, 93, 94, 95, 96, 97, 98, 99, 100]
-
-const secondList = [0, 1, 40, 44, 53, 62, 80, 92, 99, 123, 124]
-
-const thirdList = ['Ana', 'Carlos', 'Dora', 'Guilherme', 'Ingrid', 
-'Joelma', 'Joelson', 'Kaio', 'Leonardo', 'Lucas', 'Marcos', 
-'Orlando', 'Richard', 'Sabrina', 'Willamy']
-
-// unordered lists
-
-const fourList = [22, 14, 55, 21, 11, 3, 1, 11, 2, 5, 6, 8, 9, 3, 2]
-
-const fiveList = [5, 4, 3, 2, 1]
-
-const sixList = ["Zebra", "Wolf", "Lion", "Monkey", "Cat"]
-
 function searchLow(list, length) {
     let lowIndex = 0
     let low = list[lowIndex]
@@ -48,8 +23,6 @@ function selectionSort(list) {
     return newList
 }  
 
-console.log(selectionSort(sixList))
-
 function binarySearch(list, target) {
     let left = 0
     let right = list.length - 1
@@ -68,9 +41,6 @@ function binarySearch(list, target) {
             right = middlePosition - 1
         }
     }
-    return `${target} don't exists`
+    return -1 
 }
 
-console.log(binarySearch(list, 123))
-console.log(binarySearch(secondList, 80))
-console.log(binarySearch(thirdList, "Sabrina"))
