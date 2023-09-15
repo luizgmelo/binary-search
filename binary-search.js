15f4504d62d63a51624881b8d9b1c50ad8f7499e
Luiz Guilherme <luizgmelo64@gmail.com>
2023-09-15 10:55:08 -0300

    separe algorithm Selection Sort from Binary Search

--- binary-search.js
+++ binary-search.js
@@ -1,28 +1,3 @@
-function searchLow(list, length) {
-    let lowIndex = 0
-    let low = list[lowIndex]
-
-    for (let i = 0; i < length; i++) {
-        if (list[i] < low) {
-            lowIndex = i
-            low = list[lowIndex]
-        }
-    }
-    return lowIndex
-}
-
-function selectionSort(list) {
-    const newList = []; 
-    const listLength = list.length
-
-    for (let j = 0; j < listLength; j++) {
-        let index = searchLow(list, listLength)
-        let lowElement = list.splice(index, 1)[0]
-        newList.push(lowElement)  
-    }
-    return newList
-}  
-
 function binarySearch(list, target) {
     let left = 0
     let right = list.length - 1
